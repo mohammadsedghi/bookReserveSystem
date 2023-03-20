61c8d79dc10a5039f742fbf817efa223cafb9eea
mohammad <mohammadsedghi1993@gmail.com>
2023-03-20 18:55:50 +0330

    setter ,getter and constructor is created in class author

--- src/main/java/ir/bookReserveSystem/entity/Author.java
+++ src/main/java/ir/bookReserveSystem/entity/Author.java
@@ -4,4 +4,42 @@ public class Author {
     String name,family;
     int age;
     Book[] books=new Book[10];
+
+    public Author(String name, String family, int age) {
+        this.name = name;
+        this.family = family;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Book[] getBooks() {
+        return books;
+    }
+
+    public void setBooks(Book[] books) {
+        this.books = books;
+    }
 }
