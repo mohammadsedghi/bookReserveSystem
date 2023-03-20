d92c987402678cc2f6c83d05ea8d19feedfa68fb
mohammad <mohammadsedghi1993@gmail.com>
2023-03-20 21:22:23 +0330

    package and class connection , method save for author repository created

--- src/main/java/ir/bookReserveSystem/repository/AuthorRepository.java
+++ src/main/java/ir/bookReserveSystem/repository/AuthorRepository.java
@@ -1,4 +1,20 @@
 package ir.bookReserveSystem.repository;
 
+import ir.bookReserveSystem.entity.Author;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class AuthorRepository {
+    public void save(Author author) throws SQLException {
+        final String QUERY1 = "insert into author( name, family,age) VALUES (?,?,?)";
+        Connection connection = JdbcConnection.getConnection();
+        PreparedStatement statement = connection.prepareStatement(QUERY1);
+        statement.setString(1, author.getName());
+        statement.setString(2, author.getFamily());
+        statement.setInt(3,author.getAge());
+        statement.execute();
+        System.out.println("save in the database");
+    }
 }
