923e00623331042d2d2fe79fd0569f75ce9e1195
mohammad <mohammadsedghi1993@gmail.com>
2023-03-23 12:40:42 +0430

    Main class and call other class is written changes added

--- src/main/java/ir/bookReserveSystem/Main.java
+++ src/main/java/ir/bookReserveSystem/Main.java
@@ -1,7 +1,14 @@
 package ir.bookReserveSystem;
 
+import ir.bookReserveSystem.service.AuthorService;
+import ir.bookReserveSystem.service.BookService;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception{
+
+        AuthorService authorService=new AuthorService();
+        BookService bookService=new BookService();
+        authorService.register("ali","sedghi",20);
+        bookService.addBook("culture",2023,10);
     }
 }
--- src/main/java/ir/bookReserveSystem/service/BookService.java
+++ src/main/java/ir/bookReserveSystem/service/BookService.java
@@ -7,7 +7,14 @@ public class BookService {
     public final BookRepository bookRepository=new BookRepository();
     public void addBook(String title,int printYear,int authorId) throws Exception{
         Book book=new Book(title,printYear,authorId);
+        book.setBookId(bookIdGenerator(authorId));
+        book.setAuthorNameFamily("alisedghi");
         bookRepository.save(book);
+    }
+    public long  bookIdGenerator(int authorId){
+        authorId+=1;
+       long generator=authorId;
+       return generator;
     }
 
 }
