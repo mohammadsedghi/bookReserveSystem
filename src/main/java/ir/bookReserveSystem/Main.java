d4cf67acaa2f77b53555e7d8a66bcec8a7dd742f
mohammad <mohammadsedghi1993@gmail.com>
2023-03-24 12:24:05 +0430

    Corrected wrong items

--- src/main/java/ir/bookReserveSystem/Main.java
+++ src/main/java/ir/bookReserveSystem/Main.java
@@ -11,11 +11,18 @@ public class Main {
 
         AuthorService authorService=new AuthorService();
         BookService bookService=new BookService();
-       authorService.register("ali","sedghi",20);
-       bookService.addBook("economy",2023,1);
-        for (Book b:bookService.authorBookList(1)
+      // authorService.register("ali","sedghi",20);
+     //  authorService.register("reza","rezaee",30);
+       String[] title={"culture","economy","social","math","history"};
+        for (int j = 0; j < 5; j++) {
+            int i=2016;
+            bookService.addBook(title[j], i+j, 2);
+
+          }
+        for (Book b:bookService.authorBookList(2)
              ) {
             if (b!=null) System.out.println(b);
+
         }
 
     }
--- src/main/java/ir/bookReserveSystem/service/BookService.java
+++ src/main/java/ir/bookReserveSystem/service/BookService.java
@@ -8,35 +8,38 @@ import ir.bookReserveSystem.repository.BookRepository;
 import java.sql.*;
 
 public class BookService {
-    public final BookRepository bookRepository=new BookRepository();
-    public final AuthorRepository authorRepository=new AuthorRepository();
-    public void addBook(String title,int printYear,long authorId) throws Exception{
+    public final BookRepository bookRepository = new BookRepository();
+    public final AuthorRepository authorRepository = new AuthorRepository();
 
-        Book book=new Book(title,printYear,authorId);
+    public void addBook(String title, int printYear, long authorId) throws Exception {
+
+        Book book = new Book(title, printYear, authorId);
         book.setBookId(bookIdGenerator());
-       Author author= authorRepository.load(authorId);
-       String str=author.getName().concat(author.getFamily());
+        Author author = authorRepository.load(authorId);
+        String str = author.getName().concat(author.getFamily());
         book.setAuthorNameFamily(str);
         bookRepository.save(book);
     }
-    public long  bookIdGenerator()throws Exception{
-       long generator= bookRepository.loadId()+1;
-       return generator;
+
+    public long bookIdGenerator() throws Exception {
+        long generator = bookRepository.loadId() + 1;
+        return generator;
     }
+
     public Book[] authorBookList(long authorId) throws Exception {
         BookRepository bookRepository = new BookRepository();
         Book[] books = bookRepository.loadAll();
         Book[] authorBooks = new Book[books.length];
+        int counter = 0;
         for (int i = 0; i < books.length; i++) {
             if (books[i].getAuthorId() == authorId) {
-                for (int j = 0; j < authorBooks.length; j++) {
-                    authorBooks[j] = books[i];
+                authorBooks[counter] = books[i];
+                counter++;
 
-                }
-            }
         }
-        return authorBooks;
     }
+        return authorBooks;
+}
 
 
 }
