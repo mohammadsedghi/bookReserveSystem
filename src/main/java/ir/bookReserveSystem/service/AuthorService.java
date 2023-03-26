4c521d71e01195910b4e636eb4b9b7f292645ebf
mohammad <mohammadsedghi1993@gmail.com>
2023-03-26 13:46:29 +0430

    Comment written in authorService class

--- src/main/java/ir/bookReserveSystem/service/AuthorService.java
+++ src/main/java/ir/bookReserveSystem/service/AuthorService.java
@@ -8,7 +8,7 @@ import java.util.regex.Pattern;
 
 public class AuthorService {
     private final AuthorRepository authorRepository = new AuthorRepository();
-
+//before register author check name that lowercase or no then call save method to register it
     public void register(String name, String family, int age) throws Exception {
         if (Pattern.matches("[a-z]*",name)){
         Author author = new Author(name, family, age);
@@ -18,12 +18,13 @@ public class AuthorService {
             System.out.println("your name must be written lowercase , please Run program again");
             System.exit(0);}
     }
-
+//generate id for author
     public long generateAuthorId()throws Exception {
         long generator= authorRepository.loadId()+1;
         if (generator==0)generator=1;
         return generator;
     }
+    //after load author from database sort family of them
 public void sortAuthorFamily() throws Exception{
    CreatePrintList createPrintList=new CreatePrintList();
    createPrintList.sortAuthor(authorRepository.loadAll());
